d891244ed3a708ce0ffe45172daed90ce9cac424
Daniel Tull <dt@danieltull.co.uk>
2012-08-12 18:14:35 +0100

    Initial Commit

---
+++ LibA/LibA.h
@@ -0,0 +1,13 @@
+//
+//  LibA.h
+//  LibA
+//
+//  Created by Daniel Tull on 12.08.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibA : NSObject
+
+@end
---
+++ LibA/LibA.m
@@ -0,0 +1,13 @@
+//
+//  LibA.m
+//  LibA
+//
+//  Created by Daniel Tull on 12.08.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "LibA.h"
+
+@implementation LibA
+
+@end
